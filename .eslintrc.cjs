13c65e1bab82be9acdfa62323d36d5f6ed6b925e
dymafr <contact@dyma.fr>
2023-04-30 21:45:25 +0200

    maj last

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,12 +6,11 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript/recommended',
-    '@vue/eslint-config-prettier',
-    '@vue/typescript/recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
-  env: {
-    'vue/setup-compiler-macros': true,
+  parserOptions: {
+    ecmaVersion: 'latest',
   },
   rules: {
     'vue/multi-word-component-names': 'off',
